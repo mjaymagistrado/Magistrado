e7b872d2f0d7a40f4317c1139efa81a765bdf694
mjaymagistrado <mmagistrado@gbox.ncf.edu.ph>
2024-10-15 02:55:39 -0800

    Update deptController.js

--- controllers/deptController.js
+++ controllers/deptController.js
@@ -1,88 +1,83 @@
 const pool = require('../config/database');
 
-const getAllDept = async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT dept_id, dept_code, dept_name, created_at, updated_at FROM departments');
-    res.json(rows);
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-const getDeptById = async (req, res) => {
-  const { dept_id } = req.params;
-
-  try {
-    const [rows] = await pool.query('SELECT dept_id, dept_code, dept_name, created_at, updated_at FROM departments WHERE dept_id = ?', [dept_id]);
-
-    if (rows.length === 0) {
-      return res.status(404).json({ error: 'The department can not be found' });
-    }
-
-    res.json(rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-const createDept = async (req, res) => {
-    const { dept_code, dept_name } = req.body;
-  
+const getAllDepartment = async (req, res) => {
     try {
-      const [result] = await pool.query('INSERT INTO departments (dept_code, dept_name) VALUES (?, ?)', [dept_code , dept_name]);
-      res.status(201).json({ dept_id: result.insertId, dept_code, dept_name });
+        const [rows] = await pool.query('SELECT dept_id, dept_code, dept_name, created_at, updated_at FROM Department'); 
+        res.status(200).json(rows);
+        
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
-  };
-  
-  const updateDept = async (req, res) => {
+};
+
+const getDeptByid = async (req, res) => {
+    const { dept_id } = req.params;
+   
+    try {
+        const [rows] = await pool.query('SELECT dept_id, dept_code, dept_name, created_at, updated_at FROM Department WHERE dept_id = ?', [dept_id]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'Dept not found' });
+        }
+
+        res.json(rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const createDepartment = async (req, res) => {
+    const { dept_code, dept_name } = req.body;
+    try {
+        const [result] = await pool.query('INSERT INTO Department (dept_code, dept_name) VALUES (?, ?)', [dept_code, dept_name]); 
+        res.status(201).json({ dept_id: result.insertId, dept_code, dept_name }); 
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const updateDepartment = async (req, res) => {
     const { dept_id } = req.params;
     const { dept_code, dept_name } = req.body;
 
-    if (!dept_id || isNaN(Number(dept_id))) {
-        return res.status(400).json({ error: 'Invalid or missing ID' });
-      }
-
-      if (!dept_code || !dept_name) {
-        return res.status(400).json({ error: 'department code/name are required' });
-      }
-      
-    try {
-      const [result] = await pool.query('UPDATE departments SET dept_code = ?, dept_name = ? WHERE dept_id = ?', [dept_code, dept_name, dept_id]);
-  
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ error: 'Can not update, department can not be found' });
-      }
-  
-      res.json({ message: 'department updated successfully' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+    // Check if dept_id is valid and not null
+    if (!dept_id) {
+        return res.status(400).json({ error: 'Invalid department ID' });
     }
-  };
 
-  const deleteDept = async (req, res) => {
+    try {
+        const [result] = await pool.query('UPDATE Department SET dept_code = ?, dept_name = ? WHERE dept_id = ?', [dept_code, dept_name, dept_id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'Dept not found' });
+        }
+
+        res.json({ message: 'Department updated successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const deleteDepartment = async (req, res) => {
     const { dept_id } = req.params;
 
-    if (!dept_id || isNaN(Number(dept_id))) {
-        return res.status(400).json({ error: 'Invalid or missing ID' });
-      }
-
-  
-    try {
-      const [result] = await pool.query('DELETE FROM departments WHERE dept_id = ?', [dept_id]);
-  
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ error: 'Department can not be found' });
-      }
-  
-      res.json({ message: 'The department has been deleted successfully' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+    // Check if dept_id is valid and not null
+    if (!dept_id) {
+        return res.status(400).json({ error: 'Invalid department ID' });
     }
-  };
-  
-  module.exports = { getAllDept, getDeptById, createDept, updateDept, deleteDept };
 
-  
+    try {
+        const [result] = await pool.query('DELETE FROM Department WHERE dept_id = ?', [dept_id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'Dept not found' });
+        }
+
+        res.json({ message: 'Department deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+module.exports = { getAllDepartment, getDeptByid, createDepartment, updateDepartment, deleteDepartment };
